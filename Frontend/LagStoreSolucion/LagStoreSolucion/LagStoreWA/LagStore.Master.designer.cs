f62a8be4873161a43a7cd854d6365d8dc4408d8b
rio <rio88920@gmail.com>
2025-06-29 17:11:09 -0500

    Add Recarga menu item visibility logic for Jugador session

--- Frontend/LagStoreSolucion/LagStoreSolucion/LagStoreWA/LagStore.Master.designer.cs
+++ Frontend/LagStoreSolucion/LagStoreSolucion/LagStoreWA/LagStore.Master.designer.cs
@@ -60,6 +60,15 @@ namespace LagStoreWA
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl liBiblioteca;
 
         /// <summary>
+        /// Control liRecarga.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liRecarga;
+
+        /// <summary>
         /// Control liOrdenaJuego.
         /// </summary>
         /// <remarks>
